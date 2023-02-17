247518bdb470e54a84d85187dcfd6521aaa6bf52
hazywho <116442947+hazywho@users.noreply.github.com>
2023-02-17 11:18:45 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,3 +2,4 @@ basic.forever(function () {
     pins.analogWritePin(AnalogPin.P1, pins.analogReadPin(AnalogPin.P0))
     basic.showNumber(pins.analogReadPin(AnalogPin.P0))
 })
+bro i didnt copy finish help
